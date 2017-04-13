226d86d9b10cc1af140595c55c9b54c0b46c2c03
Yudi Nurhandi <yudi.nurhandi@nusa.net.id>
2017-04-13 10:07:16 +0700

    upload

--- konfig.php
+++ konfig.php
@@ -2,7 +2,7 @@
 $name="localhost";
 $user="nusanet-jkt";
 $pass="Hmxd8R6FbKdrqfMT";
-$db="dbblast";
+$db="blastgroovy";
 $connection = mysql_connect($name,$user,$pass) or die(mysql_error());
 mysql_select_db($db,$connection) or die(mysql_error());
 ?>
